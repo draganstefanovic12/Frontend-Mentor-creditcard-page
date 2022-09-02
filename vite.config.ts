f92dbf3c861791954a3b44eaec68c34705a956b4
draganstefanovic12 <stefanovicdragan112@gmail.com>
2022-09-02 16:43:13 +0200

    vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/frontend-mentor-creditcard-page/",
+  base: "/Frontend-Mentor-creditcard-page/",
   plugins: [react()],
 });
